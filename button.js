f3e04bc15d3fb84be25cef48e00fe103c18f5178
bacm852 <pimxxx51@gmail.com>
2026-02-06 19:43:14 +0700

    Update button.js

--- button.js
+++ button.js
@@ -21,7 +21,7 @@ const ASSETS = [
     desc: "Custom Roblox chat UI.",
     tags: ["UI"],
     icon: "💬",
-    url: "https://drive.google.com/uc?id=10U0if4VIyn40YLzSLGh3Xe6OJBM7wtT7&export=download"
+    url: "https://drive.usercontent.google.com/u/0/uc?id=10U0if4VIyn40YLzSLGh3Xe6OJBM7wtT7&export=download"
   }
 ];
 
